336eef811bf3f46cf47d412a7347776201c27393
reveles23 <124103604+reveles23@users.noreply.github.com>
2023-09-22 08:22:04 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>León, Guanajuato</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>León, Guanajuato</h1>
+    </header>
+    <div class="container">
+        <h2>Información General</h2>
+        <p>León es una ciudad ubicada en el estado de Guanajuato, en México. Es conocida por ser una de las ciudades más grandes e importantes de la región y por su rica historia y cultura.</p>
+        
+        <h2>Atracciones Turísticas</h2>
+        <ul>
+            <li>Parque Metropolitano de León</li>
+            <li>Basílica Catedral de León</li>
+            <li>Templo Expiatorio del Sagrado Corazón de Jesús</li>
+            <li>Plaza Mayor</li>
+        </ul>
+
+        <h2>Gastronomía</h2>
+        <p>La gastronomía de León es famosa por sus platillos típicos, como las carnitas, el pozole, los tacos de guisado y el atole. Además, la ciudad es conocida por su producción de calzado de alta calidad.</p>
+    </div>
+</body>
+</html>
